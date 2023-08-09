90224424ddf411d9073e5703c0761876a43a263f
iamsabet <iamsabet@gmail.com>
2023-08-09 20:18:11 +0330

    fix

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,8 +1,16 @@
 import { AccountProfile } from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 const Page = async () => {
   const user = await currentUser();
-  const userInfo = {};
+  if (!user) return null;
+  const userInfo = await fetchUser(user.id);
+  if (userInfo && userInfo.onboarded) return redirect("/");
+
+  // TODO: on update username must update the username with Clerk as well
+  // or else its gonna go out of sync
+
   const userData = {
     id: user?.id,
     objectId: userInfo?.id,
