ac2f813967eaf0f42b92059d2b5339850f2ec07c
iamsabet <iamsabet@gmail.com>
2023-08-10 00:00:05 +0330

    fixed wierd bugg

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
             </section>
             <RightSidebar />
           </main>
-          {children}
           <BottomBar />
         </body>
       </html>
