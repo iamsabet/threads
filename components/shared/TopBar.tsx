646f7b0cb9ac784fea3749b3328297e68bd1ec1b
iamsabet <iamsabet@gmail.com>
2023-08-10 09:34:47 +0330

    changed favicon and logo

--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -9,7 +9,7 @@ const TopBar = () => {
   return (
     <nav className="topbar">
       <Link href="/" className="flex flex-row items-center gap-4">
-        <Image src="/assets/logo.svg" alt="logo" width="28" height="28" />
+        <Image src="/assets/logo.png" alt="logo" width="32" height="32" />
         <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
       </Link>
       <div className="flex flex-row items-center gap-1">
