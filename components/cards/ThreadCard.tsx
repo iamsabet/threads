a77a41cb202b2c9925e29f66ac1434fb1b8815cc
iamsabet <iamsabet@gmail.com>
2023-08-11 03:32:53 +0330

    fixed, created at & also replies profile pics added

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -40,11 +40,30 @@ const ThreadCard = ({
   votes,
   myVote,
 }: ThreadProps) => {
-  const createdAtFormatted = createdAt.split(".")[0].split("T").join(" ");
+  const formattedDateString = (date: string) => {
+    const newDate = new Date(date);
+
+    const options = {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+      month: "short",
+      year: "numeric",
+    };
+
+    return (
+      newDate
+        // @ts-ignore
+        .toLocaleString("en-US", options)
+        .split(", ")
+        .reverse()
+        .join(", ")
+    );
+  };
   return (
     <article
       className={`flex w-full flex-col rounded-xl
-    ${isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"} `}
+    ${isComment ? "px-0 xs:px-7 mt-5" : "bg-dark-2 p-7"} `}
     >
       <div className="flex justify-between items-start">
         <div className="flex w-full flex-1 flex-row gap-4">
@@ -69,7 +88,7 @@ const ThreadCard = ({
             <p className="mt-2 text-small-regular text-light-2">{content}</p>
             <div
               className={`${
-                isComment && "gap-3 mb-8"
+                isComment && "gap-3 mb-2"
               } mt-4 flex flex-col gap-3`}
             >
               <div className="flex flex-row gap-3.5">
@@ -107,22 +126,45 @@ const ThreadCard = ({
                   className="cursor-pointer object-contain transition-all duration-150 ease-in-out hover:scale-110"
                 />
               </div>
-              {
-                // isComment &&
-                comments.length > 0 && (
-                  <Link href={`/thread/${id}`}>
-                    <p className="text-subtle-medium text-gray-1">
-                      {comments.length} replies
-                    </p>
-                  </Link>
-                )
-              }
+              {/* {isComment && comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className="mt-1 text-subtle-medium text-gray-1">
+                    {comments.length} repl{comments.length > 1 ? "ies" : "y"}
+                  </p>
+                </Link>
+              )} */}
             </div>
           </div>
         </div>
       </div>
+      {comments.length > 0 && (
+        <div
+          className={`${
+            comments.length === 1 ? "ml-2.5" : "ml-1"
+          } mt-3 flex items-center gap-2`}
+        >
+          {comments.slice(0, 2).map((comment, index) => (
+            <Image
+              key={index}
+              src={comment.author.image}
+              alt={`user_${index}`}
+              width={24}
+              height={24}
+              className={`${index !== 0 && "-ml-5"} rounded-full object-cover`}
+            />
+          ))}
+
+          <Link href={`/thread/${id}`}>
+            <p className="mt-1 text-subtle-medium text-gray-1">
+              {comments.length} repl{comments.length > 1 ? "ies" : "y"}
+            </p>
+          </Link>
+        </div>
+      )}
       <div className="">
-        <h4 className="text-light-3 text-base-semibold">{createdAt}</h4>
+        <h5 className="text-subtle-medium text-gray-1 mt-3">
+          {formattedDateString(createdAt)}
+        </h5>
       </div>
     </article>
   );
