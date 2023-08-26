d02d8b3dbf786136a37531282c42f6cd02461586
iamsabet <iamsabet@gmail.com>
2023-08-26 20:00:30 +0330

    error show verify page

--- components/auth/forms/VerofifyForm.tsx
+++ components/auth/forms/VerofifyForm.tsx
@@ -15,15 +15,21 @@ import { VerifyValidation } from "@/lib/validations/verify";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ChevronDown } from "lucide-react";
 import Link from "next/link";
-import { ChangeEvent, useRef, useState } from "react";
+import { ChangeEvent, RefObject, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
-import { IoEye, IoEyeSharp } from "react-icons/io5";
-import { RiEyeFill, RiEyeOffFill } from "react-icons/ri";
+import { IoEye, IoEyeSharp, IoWarning, IoWarningSharp } from "react-icons/io5";
+import {
+  RiErrorWarningFill,
+  RiErrorWarningLine,
+  RiEyeFill,
+  RiEyeOffFill,
+} from "react-icons/ri";
 import { z } from "zod";
 
 const VerifyForm = () => {
   const [loading, setLoading] = useState(false);
-  const [showPass, setShowPass] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const [error, setError] = useState(false);
 
   const form = useForm({
     resolver: zodResolver(VerifyValidation),
@@ -53,11 +59,12 @@ const VerifyForm = () => {
         const next = inputRefs[idx + 1].current;
         if (next) {
           next.focus();
+          verifyRequest();
         }
       }
-    }
-    if (idx + 1 === inputRefs.length) {
+    } else if (idx + 1 === inputRefs.length) {
       inputRefs[idx].current?.blur();
+      verifyRequest();
     }
   };
   // @ts-ignore
@@ -80,18 +87,43 @@ const VerifyForm = () => {
       }
     }
   };
+  const handleResend = () => {
+    setCounter((_) => 30);
+    const interVal = setInterval(() => {
+      setCounter((_prevCounter) => {
+        if (_prevCounter - 1 === 0) {
+          clearInterval(interVal);
+        }
+        return _prevCounter - 1;
+      });
+    }, 1000);
+  };
+  const checkVerificationsComplete = () => {
+    let verificationCode = "";
+    inputRefs.forEach((ref: RefObject<HTMLInputElement>) => {
+      if (ref.current?.value && ref.current?.value.length === 1) {
+        verificationCode += ref.current?.value;
+      } else {
+        return null;
+      }
+    });
+
+    return verificationCode.length === 6 ? verificationCode : null;
+  };
+  const verifyRequest = async () => {
+    const verificationCode = checkVerificationsComplete();
+    if (verificationCode) {
+      setLoading((_) => true);
+      console.log("verificationCode :" + verificationCode);
+      setTimeout(() => {
+        setLoading((_) => false);
+      }, 1500);
+    }
+  };
+
   const onSubmit = async (values: z.infer<typeof VerifyValidation>) => {
     // console.log(values.username + "/" + values.email + "/" + values.password);
     setLoading((_) => true);
-    // await createThread({
-    //   text: values.thread,
-    //   author: userId,
-    //   communityId: null,
-    //   path: pathname,
-    //   repost: null,
-    // });
-    //
-    // use login server action
     setTimeout(() => {
       setLoading((_) => false);
     }, 2000);
@@ -99,13 +131,12 @@ const VerifyForm = () => {
 
   return (
     <div className="w-full">
-      <div></div>
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
           className="flex flex-col gap-5"
         >
-          <div className="flex flex-row justify-between">
+          <div className="flex flex-row justify-start items-center gap-5">
             <div className="flex flex-row justify-between gap-2">
               {keyItems.map((item, index) => {
                 const itemStr = item.toString() as
@@ -146,26 +177,34 @@ const VerifyForm = () => {
                 );
               })}
             </div>
+            <div className="text-center my-0 py-0 h-8 mt-5">
+              {loading && <Spinner />}
+            </div>
           </div>
-          {/* <div className="text-center my-0 py-0 h-8 mt-5">
-            {loading && <Spinner />}
-          </div>
-          <p className="text-gray-2 text-[13px]">
-            Have an account?{" "}
-            <Link href="/login" className="form-link">
-              Sign In
-            </Link>
-          </p> */}
-          {/* <Button
-            className="rounded-md bg-primary-500 hover:bg-secondary-500 text-light-1 hover:text-dark-1 
-                transition-colors duration-150 ease-in-out shadow-md"
-            disabled={loading}
-            type="submit"
-          >
-            Continue
-          </Button> */}
         </form>
+        <div
+          className={`flex justify-start gap-1 items-center transition-all duration-150 ease-in-out text-red-600 ${
+            error ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          <span className="text-body-normal">
+            <RiErrorWarningFill />
+          </span>
+          <span className="text-small-medium">Incorrect code</span>
+        </div>
       </Form>
+      <button
+        disabled={counter > 0}
+        className={`text-small-regular mt-8 form-link h-5 ${
+          counter ? "opacity-50 hover:text-primary-500" : ""
+        }`}
+        // href="?"
+        onClick={(e) => {
+          handleResend();
+        }}
+      >
+        Didn't receive a code? Resend {counter ? `(${counter})` : ""}
+      </button>
     </div>
   );
 };
