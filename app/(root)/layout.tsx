304cd04a1fe220d56c5b0acb0310439334838f20
iamsabet <iamsabet@gmail.com>
2023-08-30 19:18:51 +0330

    fixes2

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -40,7 +40,7 @@ export default async function RootLayout({
             <section className="main-container">
               <div className="w-full max-w-4xl">{children}</div>
             </section>
-            {/* <RightSidebar /> */}
+            <RightSidebar />
           </main>
           <BottomBar />
           <JumpTopButton type="fixed" />
